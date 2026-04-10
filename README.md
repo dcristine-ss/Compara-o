6a94795cdcfe577bcfb4a966f40ddc9d5db29f89
dcris <dcris@192.168.0.41>
2026-04-09 21:39:46 -0300

    init: Inicio do projeto.

---
+++ README.md
@@ -0,0 +1,3 @@
+Maior entre dois números
+
+Programa em Java que lê dois números inteiros e informa qual deles é o maior ou se são iguais.
---
+++ src/main/java/com/mycompany/exercicio0012/Exercicio0012.java
@@ -0,0 +1,23 @@
+
+
+package com.mycompany.exercicio0012;
+
+
+import javax.swing.JOptionPane;
+public class Exercicio0012 {
+public static void main(String[] args) {
+int a,b;
+a = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor de a:"));
+b = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor de b:"));
+
+if(a > b){
+JOptionPane.showMessageDialog(null,"O número maior é: " + a);
+
+}else{
+if(a == b){
+JOptionPane.showMessageDialog(null,"Os números são iguais ");
+
+}else{
+JOptionPane.showMessageDialog(null,"O número maior é: " + b);
+}}
+}}
